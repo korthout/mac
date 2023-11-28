037136b214cabdd8d06bf35cb0f776ecee62ed5d
Nico Korthout <nicokorthout@gmail.com>
2023-11-28 11:12:31 +0100

    Only check lazyvim updates once per day

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local Config = require("lazy.core.config")
+Config.options.checker.frequency = 60 * 60 * 24 -- once per day
