bb7dcef2666c68b380d7937dfc7663d7f1695dd3
Nico Korthout <nicokorthout@gmail.com>
2026-03-28 09:34:42 +0100

    Fix lazyvim import order
    This imports a lazyvim.plugins.extras.* module from within your own
    plugins/ directory, which means it loads after { import = "plugins" } in
    the spec order from lazy.lua. LazyVim expects all extras imports to come
    before your custom plugins.
    
    The allowed order is:
    - `lazyvim.plugins` should be first
    - followed by any `lazyvim.plugins.extras`
    - and finally your own `plugins`

--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -13,10 +13,6 @@ if true then
       },
     },
 
-    -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
-    -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-
     {
       "nvim-treesitter/nvim-treesitter",
       opts = function(_, opts)
